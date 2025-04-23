c4c2528e83d865dc51fb5bae026312f55de58aed
JohnStuartPhil <0q5av15l@students.codeinstitute.net>
2025-04-23 17:11:08 +0100

    update band model with status

--- bands/models.py
+++ bands/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+STATUS = ((0, "Draft"), (1, "Published"))
+
 GENRE_CHOICES = (
     ('Please Select', 'Please Select'),
     ('Pop', 'Pop'),
@@ -25,15 +27,16 @@ MEMBER_CHOICES = (
 
 class Band(models.Model):
     band_name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=50, unique=True)
     genre = models.CharField(choices=GENRE_CHOICES, default='Please select',
                              unique=False)
     number_of_members = models.CharField(
         choices=MEMBER_CHOICES, default='Please select', unique=False
     )
-    slug = models.SlugField(max_length=50, unique=True)
+    review = models.TextField()
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="band_posts"
     )
-    review = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-    # status = models.IntegerField(choices=STATUS, default=0)
+    updated_on = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
