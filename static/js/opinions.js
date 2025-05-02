324e239993ce4abe798114e97d0999dc67af80f7
JohnStuartPhil <0q5av15l@students.codeinstitute.net>
2025-05-02 16:05:33 +0100

    Add initial Edit button

---
+++ static/js/opinions.js
@@ -0,0 +1,24 @@
+const editButtons = document.getElementsByClassName("btn-edit");
+const opinionText = document.getElementById("id_your_opinion");
+const opinionForm = document.getElementById("commentForm");
+const submitButton = document.getElementById("submitButton");
+
+/**
+* Initializes edit functionality for the provided edit buttons.
+* 
+* For each button in the `editButtons` collection:
+* - Retrieves the associated comment's ID upon click.
+* - Fetches the content of the corresponding comment.
+* - Populates the `commentText` input/textarea with the comment's content for editing.
+* - Updates the submit button's text to "Update".
+* - Sets the form's action attribute to the `edit_comment/{commentId}` endpoint.
+*/
+for (let button of editButtons) {
+  button.addEventListener("click", (e) => {
+    let opinionId = e.target.getAttribute("opinion_id");
+    let opinionContent = document.getElementById(`comment${opinionId}`).innerText;
+    opinionText.value = opinionContent;
+    submitButton.innerText = "Update";
+    opinionForm.setAttribute("action", `edit_comment/${opinionId}`);
+  });
+}
--- staticfiles/js/script.js
+++
@@ -1,1 +0,0 @@
-console.log("For testing purposes");
