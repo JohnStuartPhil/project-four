2b6f164a71fb136324b1ebbb0bf82aee89e05b45
JohnStuartPhil <0q5av15l@students.codeinstitute.net>
2025-05-20 23:07:59 +0100

    amendment in settings.py

--- band_house/settings.py
+++ band_house/settings.py
@@ -25,9 +25,6 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = \
- #'django-insecure-h_x0o%4h6ip*y+m@0zdjee@jpf2h%e6_xcu4=gnzupqxpkw!5k'
-
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
