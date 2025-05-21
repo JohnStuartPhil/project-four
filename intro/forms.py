3d94f0acb8d582ec1c397e0045132275c123902d
JohnStuartPhil <0q5av15l@students.codeinstitute.net>
2025-05-21 13:57:06 +0100

    Add BANDS section to Readme

--- intro/forms.py
+++ intro/forms.py
@@ -5,5 +5,5 @@ from django import forms
 class CollaborateForm(forms.ModelForm):
     class Meta:
         model = CollaborateRequest
-        fields = ('your_band_name', 'your_name', 'genre',
-                  'number_of_members', 'email', 'tell_us_about_your_band')
+        fields = ('your_band_name', 'your_name', 
+                  'email', 'tell_us_about_your_band')
